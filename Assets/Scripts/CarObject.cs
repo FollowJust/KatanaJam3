a2497416e21da62763973b496340bfdeacc6ab8b
Andrew Gladkikh <gladkikha@outlook.com>
2024-01-27 20:38:52 +0300

    some work on car

--- Assets/Scripts/CarObject.cs
+++ Assets/Scripts/CarObject.cs
@@ -4,18 +4,47 @@ using UnityEngine;
 
 public class CarObject : MonoBehaviour
 {
-    public Vector3 startPosition, targetPosition;
-
+    public Vector3 sourcePosition, targetPosition;
     public float velocity = 10.0f;
+
+    public CarObject(Vector3 start, Vector3 target)
+    {
+        sourcePosition = start;
+        targetPosition = target + (target - start); 
+    }
 
     void Start()
     {
-        
+        transform.position = sourcePosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+
+        if (TargetReached())
+        {
+
+        }
+    }
+
+    void Move()
+    {
+        float step = Time.deltaTime * velocity;
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject hit = other.gameObject;
         
+    }
+
+
+    bool TargetReached()
+    {
+        // "==" is approximate equality
+        return transform.position == targetPosition;
     }
 }
