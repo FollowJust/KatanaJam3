fcd138d2b57f684eb019bc18e3559fae5bd56c58
Georgii Varvashtian <georgii.varvashtian@pressfire.games>
2024-01-27 17:02:46 +0700

    Simple movement

--- Assets/Scripts/Bride.cs
+++ Assets/Scripts/Bride.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController), typeof(MeshFilter), typeof(MeshRenderer))]
 public class Bride : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CharacterController controller;
+    public float speed = 10.0f;
+    public float drag = 2.0f;
+    private Vector3 velocity;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        Cursor.visible = false;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Vector2 playerInput = new Vector2(-Input.GetAxis("Horizontal"), -Input.GetAxis("Vertical"));
+        Vector2.ClampMagnitude(playerInput, 1.0f);
+
+        Vector3 acceleration = speed * new Vector3(playerInput.x, 0.0f, playerInput.y) - drag * velocity;
+        velocity += acceleration * Time.deltaTime;
+        Vector3 positionDelta = Time.deltaTime * velocity + 0.5f * Time.deltaTime * Time.deltaTime * acceleration;
+
+        controller.Move(positionDelta);
     }
 }
