f4ab416eaee4d724db57a17b5e3127f0ff0c7b68
Georgii Varvashtian <georgii.varvashtian@pressfire.games>
2024-01-27 18:28:45 +0700

    Pigeon that can spawn shit

--- Assets/Scripts/DirtyObject.cs
+++ Assets/Scripts/DirtyObject.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public enum DirtyObjectType
+{ 
+    Default,
+    PigeonShit
+};
 
 public class DirtyObject : MonoBehaviour
 {
+    public DirtyObjectType type;
+
     private bool wasTriggered = false;
     public float dirtAmount = 10.0f;
+    public bool destroyAfterCollision = false;
+
+    private Vector3 velocity = new Vector3(0.0f, 0.0f, 0.0f);
 
     void Start()
     {
@@ -14,6 +26,17 @@ public class DirtyObject : MonoBehaviour
 
     void Update()
     {
+        switch (type)
+        {
+            case DirtyObjectType.PigeonShit:
+            {
+                Vector3 acceleration = new Vector3(0.0f, -9.8f, 0.0f);
+                velocity += acceleration * Time.deltaTime;
+                Vector3 positionDelta = Time.deltaTime * velocity + 0.5f * Time.deltaTime * Time.deltaTime * acceleration;
+                
+                gameObject.transform.position += positionDelta;
+            } break;
+        }
     }
 
     // WHY THE FUCK I'M DOING THIS?
---
+++ Assets/Scripts/PigeonComponent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigeonComponent : MonoBehaviour
+{
+    public GameObject shitPrefab;
+    private bool hasShitted = false;
+
+    void Start()
+    {
+        shitPrefab = GameObject.FindGameObjectWithTag("ShitPrefab");
+    }
+
+    void Update()
+    {
+        if (Time.time > 5.0f && !hasShitted)
+        {
+            Shit();
+            hasShitted = true;
+
+            print("Pigeon has shitted");
+        }
+    }
+
+    public void Shit()
+    {
+        if (shitPrefab != null) 
+        {
+            Instantiate(shitPrefab, transform.position + new Vector3(0.0f, -1.0f, 0.0f), new Quaternion());
+        }
+    }
+}
