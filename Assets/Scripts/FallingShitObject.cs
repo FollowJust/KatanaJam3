a964611a06ba8838eb018789cc779bd66c9e2e6a
aleksei.bronashko <abronashko@Saber3D.net>
2024-01-28 19:15:00 +0300

    zvuk govna

--- Assets/Scripts/FallingShitObject.cs
+++ Assets/Scripts/FallingShitObject.cs
@@ -24,6 +24,13 @@ public class ShitObject : MonoBehaviour
     {
         if (other.name != "ColliderFly" && this.transform.position.y <= 1.5f)
         {
+            var soundsArray = GetComponents<AudioSource>();
+            var rnd = Random.Range(0, soundsArray.Length - 1);
+            soundsArray[rnd].maxDistance = 5;
+            var clip = soundsArray[rnd].clip;
+
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+
             Vector3 position = this.transform.position;
             //Quaternion rotation = Quaternion.FromToRotation(Vector3.up, other.transform.eulerAngles);
             Destroy(gameObject);
