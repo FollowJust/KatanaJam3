0f62090fd4dde6a0ad3b8377834ffc98f89ca483
Gladkikh Andrei <agladkikh@SABER3D.NET>
2024-01-28 21:21:56 +0300

    added navmeshes for city

--- Assets/Scripts/CarObject.cs
+++ Assets/Scripts/CarObject.cs
@@ -2,60 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarObject : MonoBehaviour
+public class CarObject : WalkingEnemyObjectBase
 {
-    public Vector3 sourcePosition, targetPosition;
-    public float velocity = 10.0f;
-    public float soundCooldown = 100000.0f;
-    private float timeAfterSound = 0.0f;
-    public AudioClip engineSoundClip;
-
-    public CarObject(Vector3 start, Vector3 target)
-    {
-        sourcePosition = start;
-        targetPosition = target + (target - start) * 0.1f; 
-    }
+    public float maxActiveDistance = 100.0f;
+    // public Vector3 sourcePosition, targetPosition;
+    // public float velocity = 10.0f;
+    // public float soundCooldown = 100000.0f;
+    // private float timeAfterSound = 0.0f;
+    // public AudioClip engineSoundClip;
 
     void Start()
     {
-        transform.position = sourcePosition;
+        // transform.position = sourcePosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        // Move();
 
-        if (TargetReached())
+        // if (TargetReached())
+        // {
+        //     transform.position = sourcePosition;
+        // }
+
+        if (DistanceToPlayer() < maxActiveDistance)
         {
-            transform.position = sourcePosition;
+            base.Move();
         }
     }
 
-    void Move()
-    {
-        float step = Time.deltaTime * velocity;
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
-        transform.rotation = Quaternion.LookRotation(targetPosition);
+    // void Move()
+    // {
+    //     float step = Time.deltaTime * velocity;
+    //     transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+    //     transform.rotation = Quaternion.LookRotation(targetPosition);
 
-        if (engineSoundClip)
-        {
-            if (timeAfterSound > 0.001f)
-            {
-                // timeAfterSound -= Time.deltaTime * soundCooldown * 0.05f;
-            }
-            else
-            {
-                // AudioSource.PlayClipAtPoint(engineSoundClip, transform.position);
-                timeAfterSound = soundCooldown;
-            }
-        }
-    }
+    //     if (engineSoundClip)
+    //     {
+    //         if (timeAfterSound > 0.001f)
+    //         {
+    //             // timeAfterSound -= Time.deltaTime * soundCooldown * 0.05f;
+    //         }
+    //         else
+    //         {
+    //             // AudioSource.PlayClipAtPoint(engineSoundClip, transform.position);
+    //             timeAfterSound = soundCooldown;
+    //         }
+    //     }
+    // }
 
 
-    bool TargetReached()
-    {
-        // "==" is approximate equality
-        return transform.position == targetPosition;
-    }
+    // bool TargetReached()
+    // {
+    //     // "==" is approximate equality
+    //     return transform.position == targetPosition;
+    // }
 }
